7b5267276fa4fee83d1b68f53c17bbfbd88cb95e
HectorGinory <hginory@gmail.com>
2023-01-15 17:09:50 +0100

    I added about section

--- scripts/menu.js
+++
@@ -1,9 +0,0 @@
-function openMenu(n) {
-    var sections = document.getElementsByClassName('section');
-    console.log(sections)
-    var actualOpened = document.getElementsByClassName('section-opened')
-    console.log(actualOpened)
-    actualOpened[0].classList.remove("section-opened")
-    sections[n].classList.toggle("section-opened")
-}
- 
